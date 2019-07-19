cf54619c31a292d5c11a1533cf65f5dd53bb54bf
alisherKak <alisher.alish333@gmail.com>
2019-07-19 13:11:34 +0600

    Home Work 27.07.19

--- HomeWork21_07_19/Program.cs
+++ HomeWork21_07_19/Program.cs
@@ -1,15 +1,71 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace HomeWork21_07_19
 {
+    public abstract class Creator
+    {
+        public abstract IPrinter GetPrinter();
+
+        public string PrintPaper()
+        {
+            IPrinter printer = GetPrinter();
+            string result = printer.Print();
+            return result;
+        }
+    }
+
+    public class CanonCreator : Creator
+    {
+        public override IPrinter GetPrinter()
+        {
+            return new Canon();
+        }
+    }
+
+    public class HPCreator : Creator
+    {
+        public override IPrinter GetPrinter()
+        {
+            return new HP();
+        }
+    }
+
+    public interface IPrinter
+    {
+        string Print();
+    }
+
+    public class Canon : IPrinter
+    {
+        public string Print()
+        {
+            Console.WriteLine("Sheet of paper was printed by");
+            return "Canon";
+        }
+    }
+
+    public class HP : IPrinter
+    {
+        public string Print()
+        {
+            Console.WriteLine("Sheet of paper was printed by");
+            return "HP";
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
+            ExecuteCode(new CanonCreator());
+            Console.WriteLine("============");
+            ExecuteCode(new HPCreator());
+            Console.ReadLine();
+        }
+
+        static void ExecuteCode(Creator creator)
+        {
+            Console.WriteLine(creator.PrintPaper());
         }
     }
 }
